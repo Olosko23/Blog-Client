1eb3b3631a4e1bb7abd999d74a30f6780bf3ea1e
Olosko23 <oloogeorge633@gmail.com>
2024-01-03 10:22:12 +0300

    'edits'

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -10,8 +10,8 @@ const Login = () => {
   };
 
   return (
-    <div className="flex h-[77vh] md:min-h-full flex-1 flex-col justify-center mt-16 px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <div className="flex h-[77vh] md:min-h-full flex-1 flex-col justify-center mt-16 px-6 py-16 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm mt-8">
         <img className="mx-auto h-10 w-auto" src={blob2} alt="Phreddy" />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Sign in to your account
