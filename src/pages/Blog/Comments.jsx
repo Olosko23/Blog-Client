06e7aec4a3835aff4e2716aa9af3496dd24a47ef
Olosko23 <59167901+Olosko23@users.noreply.github.com>
2024-02-11 21:56:17 +0300

    Update Comments.jsx

--- src/pages/Blog/Comments.jsx
+++ src/pages/Blog/Comments.jsx
@@ -45,7 +45,7 @@ const Comments = ({ comments, articleId }) => {
   };
 
   return (
-    <div className="mt-8 w-full max-w-4xl mx-auto px-1 sm:px-6 py-2">
+    <div className="mt-8 w-full max-w-4xl mx-auto px-3 sm:px-6 py-2">
       <h2 className="text-xl mb-4 flex space-x-2 text-blue-600 font-bold">
         <span className="grid place-items-center">
           <FaCommentDots size={20} />
