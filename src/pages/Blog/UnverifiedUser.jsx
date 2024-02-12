77f82cc7bb596fc18c67075f82f5abe46cfd83ae
Olosko23 <59167901+Olosko23@users.noreply.github.com>
2024-02-12 07:11:48 +0300

    Update UnverifiedUser.jsx

--- src/pages/Blog/UnverifiedUser.jsx
+++ src/pages/Blog/UnverifiedUser.jsx
@@ -7,7 +7,7 @@ const UnverifiedUser = () => {
       <h1 className="text-4xl font-bold mb-4 text-gray-800">
         Profile Creation Required
       </h1>
-      <p className="text-gray-700 mb-8">
+      <p className="text-gray-600 mb-8">
         It seems you haven't created a profile yet. Please create your profile
         before you can start creating articles.
       </p>
